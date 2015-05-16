b11d4c32170530741dbc92533d7779c19dba6025
Ioannis Tsampoulatidis <tsampoulatidis@gmail.com>
2015-05-17 00:11:16 +0300

    clean up document ready

--- main.js
+++ main.js
@@ -1,23 +1,8 @@
 $(document).ready(function() {
 
-    $('.ui.sidebar')
-      .sidebar('attach events', '.launch.button')
+    $('.ui.dropdown')
+      .dropdown()
     ;
-    $('.filter.menu .item')
-      .tab({
-        history: false,
-    	context: '#menu-one'
-    	})
-    ;    
-    $('.secondary.menu .item')
-      .tab({
-        history: false,
-    	  context: '#menu-two'
-      });    
-	  $('.ui.about.modal').modal();
-
-
-
 
     var validationRules = {
         company: {
@@ -42,13 +27,6 @@ $(document).ready(function() {
       }
     ;
 
-    $('.ui.dropdown')
-      .dropdown({
-        on: 'hover'
-      })
-    ;
-    
-
     $('.ui.form')
       .form(validationRules, {
         inline: true,
@@ -56,34 +34,8 @@ $(document).ready(function() {
       })
     ;
 
-    $('.ui.addnew.form')
-      .form(validationRules, {
-        inline: true,
-        on: 'blur'
-      })
-    ;
-
-
     $('.masthead .information')
       .transition('scale in')
     ;
-
-    $('.about').click(function(){
-        $('.ui.about.modal')
-  			.modal('show')
-		;
-    });
-
-
-
-
-	$('.extract').load(function(){
-	   var w =    $(this).width();
-	   var h =    $(this).height();
-	   if(w < 150)
-	   		$(this).remove();	
-	}).error(function (){
-	   $(this).remove();//remove image if it fails to load// or what ever u want
-	});
 
 });
